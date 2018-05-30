969c44101b7357966d934bc5e367bfc760d35e44
Francis Tseng <f+accounts@frnsys.com>
2018-05-30 17:21:43 +0100

    working on mention-textarea re-focusing

--- main.js
+++ main.js
@@ -23,6 +23,8 @@ class Editor extends Component {
       caretPosition: null,
       focusedWord: null
     };
+
+    this.textarea = React.createRef();
   }
 
   queryMention(q) {
@@ -33,13 +35,19 @@ class Editor extends Component {
     //   this.setState({mentionResults: results});
     // });
     let results = [{
+      id: 0,
       title: 'Test Block',
+      slug: 'test-block',
       base_class: 'Block'
     }, {
+      id: 1,
       title: 'Test Channel',
+      slug: 'test-channel',
       base_class: 'Channel'
     }, {
+      id: 2,
       title: 'Test User',
+      slug: 'test-user',
       base_class: 'User'
     }];
     this.setState({mentionResults: results});
@@ -52,11 +60,13 @@ class Editor extends Component {
     let focusedWordStart = /\S+$/.exec(ev.target.value.slice(0, ev.target.selectionEnd)) || [''];
     let focusedWordEnd = /^\S+/.exec(ev.target.value.slice(ev.target.selectionEnd)) || [''];
     let focusedWord = `${focusedWordStart}${focusedWordEnd}`;
+    let focusedWordStartPos = ev.target.selectionEnd - focusedWordStart[0].length;
+    let focusedWordEndPos = ev.target.selectionEnd + focusedWordEnd[0].length;
 
     // get caret position
     // and focused word position
     let caret = getCaretCoordinates(ev.target, ev.target.selectionEnd);
-    let caretWordStart = getCaretCoordinates(ev.target, ev.target.selectionEnd - focusedWordStart[0].length);
+    let caretWordStart = getCaretCoordinates(ev.target, focusedWordStartPos);
 
     // in mention mode if focused word starts with '@'
     let mentionMode = focusedWord[0] === '@';
@@ -71,6 +81,8 @@ class Editor extends Component {
         left: caretWordStart.left + 25 // TODO compute this width shift
       },
       focusedWord: focusedWord,
+      focusedWordStart: focusedWordStartPos,
+      focusedWordEnd: focusedWordEndPos,
       mentionMode: mentionMode
     });
   }
@@ -100,13 +112,41 @@ class Editor extends Component {
     let afterLines = lines.slice(focusLineEnd, lines.length);
     let focusLine = lines.slice(focusLineStart-1, focusLineEnd);
 
+    let selectionStart = textarea.selectionStart;
+    let selectionEnd = textarea.selectionEnd;
+
     this.setState({
       focusLineStart,
       focusLineEnd,
       focusLine,
       beforeLines,
       afterLines,
+      selectionStart,
+      selectionEnd
     });
+  }
+
+  selectMention(mention) {
+    console.log(mention.slug);
+    console.log(this.state);
+    let value = this.state.value;
+    let start = this.state.value.substr(0, this.state.focusedWordStart);
+    let end = this.state.value.substr(this.state.focusedWordEnd);
+    value = `${start}@${mention.slug}${end}`;
+    // this.setState({mentionMode: false, value: value});
+    this.setState({mentionMode: false, value: value});
+    this.textarea.current.focus();
+    // this.textarea.current.selectionStart = this.state.focusedWordStart;
+    // this.textarea.current.selectionEnd = this.state.focusedWordStart;
+    console.log(this.textarea.current);
+  }
+
+  componentDidUpdate() {
+    // TODO insert caret at proper location
+    // if (this.state.selectionStart) {
+    //   this.textarea.current.selectionStart = this.state.selectionStart;
+    //   this.textarea.current.selectionEnd = this.state.selectionEnd;
+    // }
   }
 
   onChange(ev) {
@@ -118,9 +158,9 @@ class Editor extends Component {
       <div id="editor">
         {this.state.mentionMode && this.state.caretPosition && (
           <ul id="mention-menu" style={this.state.caretPosition}>
-            {this.state.mentionResults.map((r, i) => {
+            {this.state.mentionResults.map((r) => {
               return (
-                <li key={i}>
+                <li key={r.id} tabIndex="-1" onClick={() => this.selectMention(r)}>
                   <figure>
                     <img src='/thumb.jpg' alt='Test Thumb' title='Test Thumb' />
                   </figure>
@@ -137,6 +177,7 @@ class Editor extends Component {
           <textarea
             name={this.props.name}
             value={this.state.value}
+            ref={this.textarea}
             onChange={this.onChange.bind(this)}
             onFocus={this.updateFocusedLine.bind(this)}
             onClick={this.updateFocusedLine.bind(this)}
