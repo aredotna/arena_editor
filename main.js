3f2ac3c19ae5adda8ea9a818509ed43841993f47
Francis Tseng <f+accounts@frnsys.com>
2018-06-17 21:32:56 +0300

    minor cleanup

--- main.js
+++ main.js
@@ -2,15 +2,14 @@ import './css/style.sass';
 import ReactDOM from 'react-dom';
 import React, {Component} from 'react';
 import ReactMarkdown from 'react-markdown';
-import ContainsMentions from './src/tooltip';
+import ContainsMentions from './src/display';
 import Editor from './src/editor';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
-      showTooltip: false
+      value: ''
     };
   }
 
@@ -22,7 +21,7 @@ class App extends Component {
 
   render() {
     return (
-      <div id="app">
+      <div id='app'>
         <Editor name='editor' mentionQueryDelay={300} onChange={this.onEditorChange.bind(this)} />
         <ContainsMentions id='preview'>
           <ReactMarkdown source={this.state.text} />
