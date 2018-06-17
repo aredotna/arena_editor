c7ef8b91c4d7a8d3a1087b5871bf8fce56d13a96
Francis Tseng <f+accounts@frnsys.com>
2018-06-17 20:54:57 +0300

    reposition tooltip if it falls outside the window

--- src/tooltip.js
+++ src/tooltip.js
@@ -3,16 +3,34 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 
 class MentionTooltip extends Component {
+  static propTypes = {
+    isVisible: PropTypes.bool.isRequired
+  }
+
+  constructor(props) {
+    super(props);
+    this.tooltip = React.createRef();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // so we can keep track of the tooltip position and size
+    if (this.props.isVisible) {
+      let rect = this.tooltip.current.getBoundingClientRect();
+      this.props.onRectChange(rect);
+    }
+  }
+
   render() {
-    let m = this.props.mention;
+    let m = this.props.mention || {};
     let meta;
-    if (m.type === Mention.Type.Channel) {
+    if (m && m.type === Mention.Type.Channel) {
       let users = [m.data.user.username];
       users = users.concat(m.data.collaborators.map((c) => c.username));
       meta = `${m.data.length} blocks; ${users.join(', ')}`;
     }
+    let display = this.props.isVisible ? 'block' : 'none';
     return (
-      <div id="tooltip" style={{top: this.props.top, left: this.props.left}}>
+      <div id="tooltip" style={{top: this.props.top, left: this.props.left, display: display}} ref={this.tooltip} >
         {m.image &&
           <figure>
               <img src={m.image} alt={m.title} title={m.title} />
@@ -68,6 +86,7 @@ class ContainsMentions extends Component {
 
             // if so, show tooltip
             this.setState({
+              hoveredEl: el,
               showTooltip: true,
               tooltipLoading: false,
               tooltipTop: el.offsetTop + el.offsetHeight + this.props.yOffset,
@@ -79,6 +98,7 @@ class ContainsMentions extends Component {
         } else if (!this.state.tooltipLoading) {
           // if done loading, show tooltip
           this.setState({
+            hoveredEl: el,
             showTooltip: true,
             tooltipTop: el.offsetTop + el.offsetHeight + this.props.yOffset,
             tooltipLeft: el.offsetLeft
@@ -94,6 +114,18 @@ class ContainsMentions extends Component {
     }
   }
 
+  // reposition the tooltip
+  // if it falls outside the window
+  onTooltipRectChange(rect) {
+    let el = this.state.hoveredEl;
+    if (rect.bottom > window.innerHeight) {
+      this.setState({ tooltipTop: this.state.hoveredEl.offsetTop - rect.height - this.props.yOffset });
+    }
+    if (rect.right > window.innerWidth) {
+      this.setState({ tooltipLeft: this.state.hoveredEl.offsetLeft - rect.width + this.state.hoveredEl.offsetWidth });
+    }
+  }
+
   // hide tooltip when focus moves elsewhere
   onBlur() {
     this.setState({ showTooltip: false });
@@ -102,7 +134,7 @@ class ContainsMentions extends Component {
   render() {
     return (
       <div className='contains-mentions' onMouseMove={this.checkHover.bind(this)} onBlur={this.onBlur.bind(this)}>
-        {this.state.showTooltip && <MentionTooltip top={this.state.tooltipTop} left={this.state.tooltipLeft} mention={this.state.tooltipMention} />}
+        <MentionTooltip top={this.state.tooltipTop} left={this.state.tooltipLeft} mention={this.state.tooltipMention} isVisible={this.state.showTooltip} onRectChange={this.onTooltipRectChange.bind(this)} />
         {this.props.children}
       </div>);
   }
